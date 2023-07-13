aee2550038d2439c18acdf13a321a55d82c5bab7
babitaism <rbabita044@gmail.com>
2023-07-13 19:22:43 +0530

    one_file_added

--- countofmatchedsuminArray.js
+++ countofmatchedsuminArray.js
@@ -1,16 +1,15 @@
 function matchedPairSum(arr, sum) {
   let len = arr.length;
   let array = [];
+  
   for (let x = 0; x < len-1; x++) {
     for (y = x+1; y < len; y++) {
       if (sum == arr[x] + arr[y]) { 
-        array.push (arr[x],arr[y]);
-        // console.log(arr[x],"x")
-        // console.log(arr[y],"y")
+        array.push([arr[x] , arr[y]])
       }
     }
   }
-  return Array;
+  return array;
 }
 
 let pairs = matchedPairSum([10, 12, 10, 15, -1, 7, 6, 5, 4, 2, 1, 1, 1],11);
